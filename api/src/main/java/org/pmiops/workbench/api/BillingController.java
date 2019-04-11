0a1319d0971ac517af47dc8406ba5992b30bb2bf
Eric Song <songe@broadinstitute.org>
2019-04-11 10:25:47 -0400

    WIP: add test query

--- api/src/main/java/org/pmiops/workbench/api/BillingController.java
+++ api/src/main/java/org/pmiops/workbench/api/BillingController.java
@@ -1,5 +1,7 @@
 package org.pmiops.workbench.api;
 
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +31,12 @@ public class BillingController implements BillingApiDelegate {
   @Override
   public ResponseEntity<Void> findUsersExceedingFreeTierBilling() {
     log.info("testing cron job");
+    QueryJobConfiguration queryConfig = QueryJobConfiguration
+        .newBuilder("SELECT project.id, SUM(cost) cost, MIN(DATE(usage_start_time)) start_time, MAX(DATE(usage_end_time)) end_time FROM `all-of-us-workbench-test-bd.billing_data.gcp_billing_export_v1_014D91_FCB792_33D2C0` GROUP BY project.id ORDER BY cost desc;")
+        .build();
+
+    TableResult result = bigQueryService.executeQuery(queryConfig);
+
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
