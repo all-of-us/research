81f098395cf2b22faa1d98aea5e9a6ccb457800a
Brian Freeman <brian.d.freeman@gmail.com>
2019-10-01 08:59:35 -0500

    test.

--- api/src/bigquerytest/java/org/pmiops/workbench/api/BigQueryTestService.java
+++ api/src/bigquerytest/java/org/pmiops/workbench/api/BigQueryTestService.java
@@ -1,6 +1,9 @@
 package org.pmiops.workbench.api;
 
 import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryException;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -13,5 +16,19 @@ public class BigQueryTestService extends BigQueryService {
 
   protected BigQuery getBigQueryService() {
     return bigquery;
+  }
+
+  /** Execute the provided query using bigquery. */
+  public TableResult executeQuery(QueryJobConfiguration query) {
+    int count = 0;
+    int maxTries = 3;
+    while (true) {
+      try {
+        return executeQuery(query, 1000L);
+      } catch (BigQueryException e) {
+        // handle exception
+        if (++count == maxTries) throw e;
+      }
+    }
   }
 }
