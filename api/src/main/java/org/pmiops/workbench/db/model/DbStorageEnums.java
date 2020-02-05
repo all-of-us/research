4f8a417a0756228296ffbed8d9e949eef77dc629
Ariel Schwartz <aschwart@broadinstitute.org>
2020-02-05 11:44:26 -0500

    stashing.

--- api/src/main/java/org/pmiops/workbench/db/model/DbStorageEnums.java
+++ api/src/main/java/org/pmiops/workbench/db/model/DbStorageEnums.java
@@ -46,7 +46,7 @@ public final class DbStorageEnums {
           .put(Authority.FEATURED_WORKSPACE_ADMIN, (short) 3)
           .put(Authority.COMMUNICATIONS_ADMIN, (short) 4)
           .put(Authority.SECURITY_ADMIN, (short) 5)
-          .put(Authority.CLUSTER_ADMIN, (short) 6)
+          .put(Authority.WORKSPACES_VIEW, (short) 6)
           .build();
 
   public static Authority authorityFromStorage(Short authority) {
